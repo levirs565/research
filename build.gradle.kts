0ecde130b6285c6c9f9c95ba99338c511b337210
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-05-23 10:11:56 +0700

    add kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,22 @@ plugins {
 group = "com.levirs"
 version = "1.0-SNAPSHOT"
 
+val kotestVersion = "4.6.0"
+
 repositories {
     mavenCentral()
 }
 
-
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test>() {
+    useJUnitPlatform()
 }
 
 application {
